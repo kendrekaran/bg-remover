fdd45e13f4b4017b1aaee837fefcbb00d9fa0088
Karan Kendre <142140048+kendrekaran@users.noreply.github.com>
2024-12-27 21:14:56 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="flex gap-4 ">
-        <Link href="https://github.com/kendrekaran/video_downloader">
+        <Link href="https://github.com/kendrekaran/bg-remover">
             <Button className="bg-white text-black px-4 py-2 rounded-3xl hover:bg-black hover:text-white hover:border">
             <FaGithub/>  
             <h2 className='hidden sm:inline-block text-xs font-semibold'>Star me on GitHub</h2>
